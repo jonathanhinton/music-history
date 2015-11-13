f2d59aab2d77fd70d54ef1b54bccbb06a6faefa4
jonathanhinton <hintonjonathan@gmail.com>
2015-11-12 18:32:13 -0600

    introduction to factories

--- newLayout/javaScripts/app.js
+++ newLayout/javaScripts/app.js
@@ -14,40 +14,74 @@ app.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-// app.controller('SongListCtrl', [
-//   "$scope",
-//   "song_service",
-//   function($scope, song_service) {
-//     song_service.getSongs().then(function(data){
-//       $scope.song_list = data;
-//       console.log($scope.song_list);
-//     })
-//   }
-//   ])
+app.factory('song_service', function($http, $q){
+  var songlist;
+  var getSongData = function(){
+    return $q(function(resolve, reject){
+      $http
+        .get('./javaScripts/songs.json')
+        .success(function(objectFromJSONFile){
+          console.log("objectFromJSONFile", objectFromJSONFile);
+          songlist = objectFromJSONFile.songs;
+          resolve(objectFromJSONFile.songs);
+        }, function(error) {
+          reject(error);
+        } //end error
+      ) //end success
+    }); //end q function
+  };
 
-app.controller('SongsCtrl', function($scope, $q, $http){
-  // $.ajax('javaScripts/songs.json')
-  // .done(function(ajaxSongs){
-  //   $scope.songs = ajaxSongs.songs;
-  //   console.log("$scope.songs", $scope.songs);
-  //   $scope.$apply();
-  // });
-  return $q(function(resolve, reject){
-    $http
-      .get('./javaScripts/songs.json')
-      .success(function(objectFromJSONFile){
-        console.log("objectFromJSONFile", objectFromJSONFile);
-        resolve(objectFromJSONFile.songs);
-      }, function(error) {
-        reject(error);
-      }
-    )
-  })
-  .then(function(jsonData){
-    console.log("jsonData", jsonData);
-    $scope.songs = jsonData;
-  });
+  function getSongs(){
+    return getSongData();
+  }
+
+  function getSingleSong(id){
+    return song_list.filter(function(song){
+      return song.id === id;
+    })[0];
+  }
+
+  function addsong(songObj){
+
+  }
+  return {
+    getSongs : getSongs,
+    getSingleSong : getSingleSong,
+    addsong : addsong
+  };
 });
+
+app.controller('SongsCtrl', [
+  "$scope",
+  "song_service",
+  function($scope, song_service) {
+    song_service.getSongs().then(function(data){
+      $scope.song_list = data;
+      console.log($scope.song_list);
+    }).catch(function(){
+      $scope.error = "Songs could not be loaded";
+    })
+  }
+  ]);
+
+// app.controller('SongsCtrl', function($scope, $q, $http){
+//   // $.ajax('javaScripts/songs.json')
+//   // .done(function(ajaxSongs){
+//   //   $scope.songs = ajaxSongs.songs;
+//   //   console.log("$scope.songs", $scope.songs);
+//   //   $scope.$apply();
+//   // });
+//   .then(function(jsonData){
+//     console.log("jsonData", jsonData);
+//     $scope.songs = jsonData;
+//   }); //end .then statement
+
+// //function for detail
+//   $scope.songInfo = function(song){
+//     console.log("song", song);
+//   };
+
+// }); //end SongsCtrl controller
 
 
 app.controller('AddSongCtrl', function($scope){
@@ -59,11 +93,12 @@ app.controller('AddSongCtrl', function($scope){
   };
 
   $scope.addSong = function(){
+    console.log("click");
     $scope.songs.$add({
       artist: $scope.newSong.artist,
       title: $scope.newSong.title,
       album: $scope.newSong.album
     });
-  };
+  }
 });
 
